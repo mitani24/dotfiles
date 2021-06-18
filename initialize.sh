04531c46634f0455608203a0ffbc03f7e20c17fc
mitani24 <h.mitani@ashita-team.com>
2021-06-19 02:37:46 +0900

    add sound files

--- initialize.sh
+++ initialize.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+cd `dirname $0`
+
 # install HomeBrew (https://brew.sh/index_ja)
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # install packages
 brew bundle --global
+
+# install sounds
+cp assets/sounds/* $HOME/Library/Sounds
