047432ff140ccc5b9b66c4ec14096546f0d14f2a
mitani24 <h.mitani@ashita-team.com>
2025-10-11 13:53:59 +0900

    update config

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -16,6 +16,24 @@ alias yw="yarn workspace"
 alias relogin="exec $SHELL -l"
 alias pn="pnpm"
 
+# Cateras
+alias cgen="yw bff codegen:local && yw frontend codegen:local"
+alias cgens="yw bff-setup codegen:local && yw frontend-setup codegen:local"
+alias clog="dc logs -f"
+alias cupf="dc up -d frontend swagger_ui_api good_job s3mock && clog"
+alias cupfm="dc up -d frontend swagger_ui_api good_job s3mock mock-backend && clog"
+alias cups="dc up -d frontend-setup swagger_ui_setup good_job s3mock && clog"
+alias cupsm="dc up -d frontend-setup swagger_ui_setup good_job s3mock mock-backend-setup && clog"
+alias cupa="dc up -d && clog"
+alias cinit="y bundle && y migrate:reset && y seed"
+alias cdepdev="git push --force origin HEAD:deploy/dev"
+alias ctoken='auth0 test token "0PZDDaUulV5cknOh1nqykJucQ1mjgAJB" -a https://api.jobaria-dev.cloud/api/ --force -s "profile" --json | jq -r ".access_token"'
+
+# Tools
+alias difit="npx difit --port 5000"
+alias ccu="npx ccusage"
+alias ccv="PORT=5001 npx @kimuson/claude-code-viewer"
+
 zle -N open-ghq-repository
 bindkey '^t' open-ghq-repository
 bindkey '^g' anyframe-widget-cd-ghq-repository
--- zsh/volta.zsh
+++ zsh/volta.zsh
@@ -1,2 +1,6 @@
 export VOLTA_HOME="$HOME/.volta"
 export PATH="$VOLTA_HOME/bin:$PATH"
+
+# pnpm support
+# https://www.voltajs.com/advanced/pnpm.html?utm_source=chatgpt.com#enabling-pnpm-support
+export VOLTA_FEATURE_PNPM=1
